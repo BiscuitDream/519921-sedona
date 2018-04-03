f440763555ab0e05b07e6e0092fc9182824bea95
Igor Pechenkin <33398265+BiscuitDream@users.noreply.github.com>
2018-04-03 04:58:10 +0300

    JS в отдельный файл

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+var toggleButton = document.querySelector(".modal-button");
+var modalSearch = document.querySelector(".modal-search");
+var modalForm = document.querySelector(".search-form");
+
+var arrival = document.querySelector("input[name=arrival]");
+var departure = document.querySelector("input[name=departure]");
+var adults = document.querySelector("input[name=adults]");
+var children = document.querySelector("input[name=children]");
+
+var isStorageSupport = true;
+var storageAdults = "";
+var storageChildren = "";
+
+try {
+storageAdults = localStorage.getItem("numberOfAdults");
+storageChildren = localStorage.getItem("numberOfChildren");
+} catch (err) {
+isStorageSupport = false;
+}
+
+toggleButton.addEventListener("click", function (evt) {
+evt.preventDefault();
+if (modalSearch.classList.contains("modal-error")) {
+    modalSearch.classList.remove("modal-error");
+}
+modalSearch.classList.toggle("modal-show");
+if (storageAdults) {
+    adults.value = storageAdults;
+}
+if (storageAdults) {
+    children.value = storageChildren;
+}
+});
+
+modalForm.addEventListener("submit", function (evt) {
+if (!arrival.value || !departure.value || !adults.value || !children.value) {
+    evt.preventDefault();
+    modalSearch.classList.remove("modal-error");
+    modalSearch.offsetWidth = modalSearch.offsetWidth;
+    modalSearch.classList.add("modal-error");
+    console.log("Нужно ввести данные в форму");
+} else {
+    if (isStorageSupport) {
+    localStorage.setItem("numberOfAdults", adults.value);
+    localStorage.setItem("numberOfChildren", children.value);
+    }
+}
+});
